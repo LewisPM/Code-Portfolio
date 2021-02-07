250246c07b2214b3e5cf2477fd9eed55b432e125
Pauline <pauline.panjo@outlook.com>
2021-02-07 16:04:59 -0500

    complete js function, highlight on navClick

--- script.js
+++ script.js
@@ -19,15 +19,46 @@ let onHover = bodyElement => {
 
 bodyElements.forEach(onHover);
 
-/*onClick Nav Body Highlight function
+/*onClick Nav Body Highlight function*/
+//figure out how to write so it is scalable
+//For now it is done for each element
 
 let navElements = document.querySelectorAll('.navText');
 
+let highlightBody = event => {
+    if (event.target.innerText === 'Bio') {
+        document.getElementById('bio').style.backgroundColor = 'lightgray';
+    } else if (event.target.innerText === 'Projects') {
+        document.getElementById('projects').style.backgroundColor = 'lightgray';
+    } else if (event.target.innerText === 'Languages') {
+        document.getElementById('languages').style.backgroundColor = 'lightgray';
+    } else if (event.target.innerText === 'Special Skills') {
+        document.getElementById('specialSkills').style.backgroundColor = 'lightgray';
+    } else if (event.target.innerText === 'Education') {
+        document.getElementById('education').style.backgroundColor = 'lightgray';
+    } else if (event.target.innerText === 'Contact') {
+        document.getElementById('contact').style.backgroundColor = 'lightgray';
+    }
+}
+
+let returnBodyColor = event => {
+    if (event.target.innerText === 'Bio') {
+        document.getElementById('bio').style.backgroundColor = '';
+    } else if (event.target.innerText === 'Projects') {
+        document.getElementById('projects').style.backgroundColor = '';
+    } else if (event.target.innerText === 'Languages') {
+        document.getElementById('languages').style.backgroundColor = '';
+    } else if (event.target.innerText === 'Special Skills') {
+        document.getElementById('specialSkills').style.backgroundColor = '';
+    } else if (event.target.innerText === 'Education') {
+        document.getElementById('education').style.backgroundColor = '';
+    } else if (event.target.innerText === 'Contact') {
+        document.getElementById('contact').style.backgroundColor = '';
+    }
+}
+
 let onNavClick = navElement => {
-    navElement.onclick = highlight;
-    navElement.onmouseup = returnColor;
+    navElement.onmousedown = highlightBody;
+    navElement.onmouseup = returnBodyColor;
 }
 navElements.forEach(onNavClick);
-
-
-*/
